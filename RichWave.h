d8813c244259a58e175241213aff3ef65dec0dca
derFrickler <johannes@hermen.net>
2016-12-01 09:02:25 +0100

    added RSSI reading

--- RichWave.h
+++ RichWave.h
@@ -7,13 +7,16 @@
 class RichWave
 {
 	public:
-	  RichWave(uint8_t dataPin, uint8_t lePin, uint8_t clkPin);
+	  RichWave(uint8_t dataPin, uint8_t lePin, uint8_t clkPin, uint8_t rssiPin);
 	  void setFrequency(int frequency);
 	  void setRegister(byte address, unsigned long data);
+          uint16_t readRSSI();
 	private:
 		uint8_t dataPin;
 		uint8_t lePin;
 		uint8_t clkPin;
+                uint8_t rssiPin;
+                uint16_t lastTuned;
 		void sendBit(boolean isOne);
 };
 
